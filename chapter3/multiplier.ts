eb6fcae9b2e494a260e779ddbdf1c793f2349ca2
Okamissu <okamissu@gmail.com>
2026-07-29 10:39:35 +0200

    refactor: change file structure

--- chapter3/multiplier.ts
+++ chapter3/multiplier.ts
@@ -1,25 +1,9 @@
-type Operation = 'multiply' | 'add' | 'divide'
-
-const calculator = (a: number, b: number, op: Operation): number => {
-  switch (op) {
-    case 'multiply':
-      return a * b
-    case 'divide':
-      if (b === 0) throw new Error("Can't divide by 0!")
-      return a / b
-    case 'add':
-      return a + b
-    default:
-      throw new Error('Operation is not multiply, add or divide!')
-  }
+const multiplicator = (a: number, b: number, printText: string) => {
+  console.log(printText, a * b)
 }
 
-try {
-  console.log(calculator(1, 5, 'divide'))
-} catch (error: unknown) {
-  let errorMessage = 'Something went wrong: '
-  if (error instanceof Error) {
-    errorMessage += error.message
-  }
-  console.log(errorMessage)
-}
+// command line arguments start from process.argv[2]
+const a: number = Number(process.argv[2])
+const b: number = Number(process.argv[3])
+
+multiplicator(a, b, `Multiplied ${a} and ${b}, the result is:`)
