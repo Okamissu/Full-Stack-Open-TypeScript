5b8d5b88e369faf38585a10eabb50733633d97a2
Okamissu <okamissu@gmail.com>
2026-08-03 14:02:31 +0200

    feat: restructure backend with diary entries API and add CORS support

---
+++ chapter4/src/services/diaryService.ts
@@ -0,0 +1,15 @@
+import diaries from '../../data/entries.ts';
+import type { DiaryEntry } from '../types.ts';
+
+const getEntries = (): DiaryEntry[] => {
+  return diaries;
+};
+
+const addDiary = () => {
+  return null;
+};
+
+export default {
+  getEntries,
+  addDiary,
+};
