90323d98a4d0289c95568d225312e43d05b03157
Okamissu <okamissu@gmail.com>
2026-08-04 13:39:54 +0200

    feat: add utility function to parse new diary entries and update diary post route

--- chapter4/src/routes/diaries.ts
+++ chapter4/src/routes/diaries.ts
@@ -1,7 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express, { type Response } from 'express';
 import diaryService from '../services/diaryService.ts';
 import type { NonSensitiveDiaryEntry } from '../types.ts';
+import parseNewDiaryEntry from '../utils.ts';
 
 const router = express.Router();
 
@@ -18,16 +18,17 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  const { date, weather, visibility, comment } = req.body;
-
-  const addedEntry = diaryService.addDiary({
-    date,
-    weather,
-    visibility,
-    comment,
-  });
-
-  res.json(addedEntry);
+  try {
+    const newDiaryEntry = parseNewDiaryEntry(req.body);
+    const addedEntry = diaryService.addDiary(newDiaryEntry);
+    res.json(addedEntry);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 export default router;
