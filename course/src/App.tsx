3654bfcba1cbf2ed8619747c810450319ff7f06c
Okamissu <okamissu@gmail.com>
2026-08-12 14:48:57 +0200

    feat: refactor App component and add new components for course structure

--- course/src/App.tsx
+++ course/src/App.tsx
@@ -1,6 +1,11 @@
+import Content from './components/Content';
+import Header from './components/Header';
+import Total from './components/Total';
+import type { CoursePart } from './types';
+
 const App = () => {
-  const courseName = 'Half Stack application development';
-  const courseParts = [
+  const courseName: string = 'Half Stack application development';
+  const courseParts: CoursePart[] = [
     {
       name: 'Fundamentals',
       exerciseCount: 10,
@@ -15,24 +20,11 @@ const App = () => {
     },
   ];
 
-  const totalExercises = courseParts.reduce(
-    (sum, part) => sum + part.exerciseCount,
-    0,
-  );
-
   return (
     <div>
-      <h1>{courseName}</h1>
-      <p>
-        {courseParts[0].name} {courseParts[0].exerciseCount}
-      </p>
-      <p>
-        {courseParts[1].name} {courseParts[1].exerciseCount}
-      </p>
-      <p>
-        {courseParts[2].name} {courseParts[2].exerciseCount}
-      </p>
-      <p>Number of exercises {totalExercises}</p>
+      <Header name={courseName} />
+      <Content courseParts={courseParts} />
+      <Total courseParts={courseParts} />
     </div>
   );
 };
---
+++ course/src/components/Content.tsx
@@ -0,0 +1,11 @@
+import type { CoursePart } from '../types';
+
+const Content = ({ courseParts }: { courseParts: CoursePart[] }) => {
+  return courseParts.map((part) => (
+    <p key={part.name}>
+      {part.name} {part.exerciseCount}
+    </p>
+  ));
+};
+
+export default Content;
