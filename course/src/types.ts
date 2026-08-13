c28372596d0e4eae4c418c98f670cceb5e6b790b
Okamissu <okamissu@gmail.com>
2026-08-13 10:56:49 +0200

    feat: update CoursePart types to include description in basic and background parts

--- course/src/types.ts
+++ course/src/types.ts
@@ -3,8 +3,11 @@ interface CoursePartBase {
   exerciseCount: number;
 }
 
-interface CoursePartBasic extends CoursePartBase {
+interface CoursePartWithDescription extends CoursePartBase {
   description: string;
+}
+
+interface CoursePartBasic extends CoursePartWithDescription {
   kind: 'basic';
 }
 
@@ -13,8 +16,7 @@ interface CoursePartGroup extends CoursePartBase {
   kind: 'group';
 }
 
-interface CoursePartBackground extends CoursePartBase {
-  description: string;
+interface CoursePartBackground extends CoursePartWithDescription {
   backgroundMaterial: string;
   kind: 'background';
 }
