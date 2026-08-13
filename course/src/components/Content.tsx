a83483ab0b9d7a62b6f487353a3cf2414b4e5d35
Okamissu <okamissu@gmail.com>
2026-08-13 11:20:59 +0200

    refactor: restructure Content component to use map for rendering course parts and enhance logging

--- course/src/components/Content.tsx
+++ course/src/components/Content.tsx
@@ -2,27 +2,47 @@ import type { CoursePart } from '../types';
 import { assertNever } from '../utils';
 
 const Content = ({ courseParts }: { courseParts: CoursePart[] }) => {
-  courseParts.forEach((part) => {
-    switch (part.kind) {
-      case 'basic':
-        console.log(part.name, part.exerciseCount, part.description);
-        break;
-      case 'group':
-        console.log(part.name, part.exerciseCount, part.groupProjectCount);
-        break;
-      case 'background':
-        console.log(part.name, part.exerciseCount, part.backgroundMaterial);
-        break;
-      default:
-        return assertNever(part);
-    }
-  });
+  return (
+    <div className="space-y-4">
+      {courseParts.map((part) => {
+        switch (part.kind) {
+          case 'basic':
+            return (
+              <div key={part.name} className="*:my-0.5">
+                <p className="font-bold">
+                  {part.name} {part.exerciseCount}
+                </p>
+                <p className="italic">{part.description}</p>
+              </div>
+            );
 
-  return courseParts.map((part) => (
-    <p key={part.name}>
-      {part.name} {part.exerciseCount}
-    </p>
-  ));
+          case 'group':
+            return (
+              <div key={part.name} className="*:my-0.5">
+                <p className="font-bold">
+                  {part.name} {part.exerciseCount}
+                </p>
+                <p>project exercises {part.groupProjectCount}</p>
+              </div>
+            );
+
+          case 'background':
+            return (
+              <div key={part.name} className="*:my-0.5">
+                <p className="font-bold">
+                  {part.name} {part.exerciseCount}
+                </p>
+                <p className="italic">Confusing description</p>
+                <p>{part.backgroundMaterial}</p>
+              </div>
+            );
+
+          default:
+            return assertNever(part);
+        }
+      })}
+    </div>
+  );
 };
 
 export default Content;
