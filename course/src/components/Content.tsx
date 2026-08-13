ff3d342f7eaba746c9d83d8d399046dd3ece66d2
Okamissu <okamissu@gmail.com>
2026-08-13 10:46:10 +0200

    feat: add exhaustive type checking utility and enhance Content component logging

--- course/src/components/Content.tsx
+++ course/src/components/Content.tsx
@@ -1,6 +1,23 @@
 import type { CoursePart } from '../types';
+import { assertNever } from '../utils';
 
 const Content = ({ courseParts }: { courseParts: CoursePart[] }) => {
+  courseParts.forEach((part) => {
+    switch (part.kind) {
+      case 'basic':
+        console.log(part.name, part.exerciseCount, part.description);
+        break;
+      case 'group':
+        console.log(part.name, part.exerciseCount, part.groupProjectCount);
+        break;
+      case 'background':
+        console.log(part.name, part.exerciseCount, part.backgroundMaterial);
+        break;
+      default:
+        return assertNever(part);
+    }
+  });
+
   return courseParts.map((part) => (
     <p key={part.name}>
       {part.name} {part.exerciseCount}
