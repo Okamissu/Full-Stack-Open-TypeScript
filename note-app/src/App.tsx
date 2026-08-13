43c5aadac8e2ba03be439d91453924806b96ba42
Okamissu <okamissu@gmail.com>
2026-08-13 16:26:12 +0200

    feat: implement note creation functionality and display notes in App component

--- note-app/src/App.tsx
+++ note-app/src/App.tsx
@@ -1,10 +1,41 @@
+interface Note {
+  id: string;
+  content: string;
+}
+
 import { useState } from 'react';
 
 const App = () => {
+  const [notes, setNotes] = useState<Note[]>([{ id: '1', content: 'testing' }]);
   const [newNote, setNewNote] = useState('');
-  const [notes, setNotes] = useState([]);
 
-  return null;
+  const noteCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const noteToAdd = {
+      content: newNote,
+      id: String(notes.length + 1),
+    };
+    setNotes((prevNotes) => [...prevNotes, noteToAdd]);
+    setNewNote('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={noteCreation}>
+        <input
+          type="text"
+          value={newNote}
+          onChange={(e) => setNewNote(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+      <ul>
+        {notes.map((note) => (
+          <li key={note.id}>{note.content}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
