137c5a7911953de979dccf02ef1e86f9dffa0bf9
Okamissu <kamil.m.kobylarz@gmail.com>
2026-08-16 11:25:22 +0200

    feat: initialize flight diaries frontend with React, Vite, and Tailwind CSS

---
+++ flightdiaries/frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="m-10 bg-blue-100 p-5 rounded-l">
+      <h1 className="font-bold text-3xl ">Flight Diaries</h1>
+    </div>
+  );
+}
+
+export default App;
